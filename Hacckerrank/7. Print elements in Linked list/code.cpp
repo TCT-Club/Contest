1a0db2228ab915db98e8e963886a0ee0244a4dc2
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-08-09 14:43:00 +0530

    Create code.cpp

---
+++ Hacckerrank/7. Print elements in Linked list/code.cpp
@@ -0,0 +1,20 @@
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+
+    SinglyLinkedListNode *temp = head;
+    
+    while(temp != NULL) {
+        cout << temp ->data << endl;
+        temp = temp->next;
+    }
+}
