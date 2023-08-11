368da25f3b660ecce1e0b4244f829c3bc77b976b
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-08-11 12:48:52 +0530

    Inserting a node at the head of linkedlist

---
+++ HackerRank-1/9. Insert a node at the head of Linkedlist/code.cpp
@@ -0,0 +1,18 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+    
+    SinglyLinkedListNode* newNode = new SinglyLinkedListNode(data);
+    newNode->next = llist;
+    llist = newNode;
+    return llist;
+}
